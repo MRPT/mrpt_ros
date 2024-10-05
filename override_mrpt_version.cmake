3ced354c284d16e30c836a4f6333b9f813014b44
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2024-10-05 07:54:13 +0200

    Add support for override_mrpt_version for local builds

---
+++ override_mrpt_version.cmake
@@ -0,0 +1,7 @@
+# This file can be used to override the default behavior of
+# checking package.xml and build the MRPT version in the
+# XML <version>X.Y.Z.</version> tag.
+# Uncomment the next line to enforce using a particular
+# MRPT version (branch name, git tag):
+
+#set(MRPT_VERSION_TO_DOWNLOAD "develop")
